d4c3171d0699821aa7003c9ef1ab4b767a301f46
Bryan_w11 <h.bryan68@yahoo.ca>
2025-08-07 12:38:48 -0400

    server connection logic

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const connectDB = require("./db");
+// cors — a Node.js package that allows cross-origin resource sharing
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+// Connect to database
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
+// Log to confirm API running on root
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(3000, () => console.log("Server Started"));
+const noteRouter = require("./routes/noteRoutes");
+app.use("/notes", noteRouter);
+
+const PORT = process.env.PORT;
+// Start Express server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
