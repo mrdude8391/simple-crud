a2c24498655dbd393f7e62f20edb6abcffcbffa8
Bryan_w11 <h.bryan68@yahoo.ca>
2025-08-14 13:51:24 -0400

    remove not service log

--- client/src/services/noteService.ts
+++ client/src/services/noteService.ts
@@ -5,9 +5,6 @@ const API_URL = import.meta.env.VITE_API_URL
 
 export const getNotes = () => axios.get(API_URL);
 export const createNote = (note: Note) => axios.post(API_URL, note);
-export const updateNote = (id: string, note: Note) => axios.patch(`${API_URL}/${id}`, note).then((response) => {
-    console.log(response)
-    console.log(response.data)
-});
+export const updateNote = (id: string, note: Note) => axios.patch(`${API_URL}/${id}`, note);
 export const deleteNote = (id: string) => axios.delete(`${API_URL}/${id}`);
 
